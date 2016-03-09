0b5f0a72a6a6d9ecd6f52c53bc915790829657b0
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-03-09 11:55:40 -0500

    styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
-var movies = ['the royal tenenbaums', 'the life aquatic with steve zissou'];
 
+// initial movie list
+var movies = ['the royal tenenbaums', 'the life aquatic with steve zissou', 'moonrise kingdom', 'taxi driver', 'jaws', 'the ring', 'inception', 'back to the future'];
+
+//create initial movie buttons
+//replace " " with "+" for address 
 for (var i = 0; i < movies.length; i++){
 		var b = $('<button>');
 		b.addClass('movie btn btn-default')
@@ -10,7 +14,7 @@ for (var i = 0; i < movies.length; i++){
 	}
 
 
-
+//create buttons
 function createBTN (){
 	$('#buttonHolder').empty();
 	for (var i = 0; i < movies.length; i++){
@@ -23,6 +27,7 @@ function createBTN (){
 	}
 }
 
+//on search: push new movies to array, create new button and clear value
 $('#movieSearchBTN').on('click', function(){
 	if ($('#movieSearch').val() != ""){
 		var movieTXT = $('#movieSearch').val().trim();
@@ -30,26 +35,29 @@ $('#movieSearchBTN').on('click', function(){
 		movies.push(newMovie);
 		createBTN();
 		$('#movieSearch').val("");
+        return false;
 	}
 });
 
+
+//send request for gifs
 $('#buttonHolder').on('click', '.movie', function(){
 	$('#gifDiv').empty();
 	var movieData = $(this).data('name');
-    var movieURL = "http://api.giphy.com/v1/gifs/search?q=" + movieData + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var movieURL = "http://api.giphy.com/v1/gifs/search?q=" + movieData + "+movie&api_key=dc6zaTOxFJmzC&limit=10";
 
 
     $.ajax({url: movieURL, method: 'GET'}).done(function(response) {
     	var results = response.data;
-
+//create div
     	for (var i = 0; i < results.length; i++){
     		var movieDiv = $('<div>');
-    		movieDiv.addClass('panel panel-default');
-
+    		movieDiv.addClass('movieHolder');
+//get movie rating
     		var movieRating = $('<div>');
-    		movieRating.addClass('panel-heading');
+    		movieRating.addClass('movieHeading');
     		movieRating.text('Rating: ' + results[i].rating);
-
+//get movie images
     		var movieImg = $('<img>');
     		movieImg.addClass('gifClass');
     		movieImg.attr('src', results[i].images.fixed_height.url);
@@ -57,22 +65,22 @@ $('#buttonHolder').on('click', '.movie', function(){
     		movieImg.attr('data-animate', results[i].images.fixed_height.url);
     		movieImg.attr('data-state', 'animate');
     		movieImg.attr('alt', 'movie gif');
-
+//append elements to movie div
     		movieDiv.append(movieRating);
     		movieDiv.append(movieImg);
 	    	$('#gifDiv').append(movieDiv);
     	}
-
+//warning message if no gifs exist
     	if(results.length == 0) {
-    		$('#gifDiv').append("<p>I'm Sorry. No gifs currently exist for this search</p><p>Please try another movie<p>");
+    		$('#gifDiv').append("<p class='warning'>I'm Sorry. No gifs currently exist for this search</p><p class='warning'>Please try another movie<p>");
     	}
     });
-
-    $(this).siblings().removeClass('active')
-    $(this).addClass('active');
+//add active class to buttons
+    $(this).siblings().removeClass('active1')
+    $(this).addClass('active1');
   });
 
-
+//pause image on click
   $('#gifDiv').on('click', '.gifClass', function(){
   	var state = $(this).attr('data-state'); 
   	if (state == 'still'){
